89fbd7f36e8df458849de2acf574e8a190b5c437
Charles Rymal <charlesrymal@gmail.com>
2014-03-13 17:48:16 -0700

    equations

--- equations/navier_stokes.py
+++ equations/navier_stokes.py
@@ -1,11 +1,31 @@
 from sympy import *
 
+# 2D
+# x-momentum
+
 t, P, n = symbols('t, P, n')
 
-u = tensor.IndexedBase('u')
-x = tensor.IndexedBase('x')
+u_w, u_e, u_s, u_n = symbols('u_w u_e u_s u_n')
+v_s, v_n = symbols('v_s v_n')
 
-i, j = map(tensor.Idx, ['i', 'j'])
+uu_w, uu_e = symbols('uu_w uu_e')
+
+u_t0, u_t1 = symbols('u_t(0:2)')
+
+u = symbols('u')
+
+dx, dy = symbols('dx dy')
+
+P_w, P_e = symbols('P_w P_e')
+
+expr = -Derivative(u,t) 
+expr += uu_w * dy - uu_e * dy + u_s * v_s * dx - u_n * v_n * dx
+expr += P_w * dy - P_e * dy
+expr += - nu * dudx_w * dy + 
+
+
+pprint(expr)
+
 
 #expr = Derivative(u[i],t) + Derivative(u[i]*u[j], x[j]) + Derivative(P, x[i])
 
