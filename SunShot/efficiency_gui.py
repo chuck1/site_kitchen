3fe284904bb0869497aa0caf6933ddb6e9111115
Charles Rymal <charlesrymal@gmail.com>
2014-08-06 20:46:02 -0700

    add controls for all parameters

--- SunShot/efficiency_gui.py
+++ SunShot/efficiency_gui.py
@@ -8,47 +8,50 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
-modules_dir = os.environ["HOME"] + "/Programming/Python/Modules"
-sys.path.append( modules_dir )
+def efficiency(T, q_abs, T_infr = 298.15, T_infc = 298.15, h_nat = 15.0, emiss = 0.95):
+	sigma = 5.67 * pow(10,-8)
+	
+	q_rad = emiss * sigma * (pow(T,4) - pow(T_infr,4))
+	q_conv = h_nat * (T - T_infc)
+	
+	# thermal
+	eff_ther = q_abs / (q_abs + q_rad + q_conv)
+	
+	# receiver
+	eff_recv = eff_ther * emiss
+	
+	return eff_ther, eff_recv, q_rad, q_conv
 
 class HelloWorld:
-
 	# This is a callback function. The data arguments are ignored
 	# in this example. More on callbacks below.
 	def hello(self, widget, data=None):
 		n = [0]
-		for i in range(1):
-			s = self.entry[i].get_text()
-			if s:
-				n[i] = float(s)
-			else:
-				print "empty string"
-				return
+		#for i in range(1):
+		#	s = self.entry[i].get_text()
+		#	if s:
+		#		n[i] = float(s)
+		#	else:
+		#		print "empty string"
+		#		return
+
 		# get entries
-		T = float(self.entry_temp.get_text())
-		q_abs = float(self.entry_flux.get_text())
+		T = float(self.entry["T"].get_text())
+		q = float(self.entry["q"].get_text())
+		Tinfr = float(self.entry["Tinf r"].get_text())
+		Tinfc = float(self.entry["Tinf c"].get_text())
+		h = float(self.entry["h"].get_text())
+		em = float(self.entry["em"].get_text())
 		
 		
-		T_inf = 25 + 273.0
-		h_nat = 15.0
-		sigma = 5.67 * pow(10,-8)
-		emiss = 0.95
-		
-		
-		q_rad = emiss * sigma * (pow(T,4) - pow(T_inf,4))
-		q_conv = h_nat * (T - T_inf)
-		
-		# thermal
-		eff_ther = q_abs / (q_abs + q_rad + q_conv)
-		
-		self.entry_ther.set_text("{0}".format(eff_ther))
-	
-		# receiver
-		eff_recv = eff_ther * emiss
-		
-		self.entry_recv.set_text("{0}".format(eff_recv))
-
-
+                eff_ther, eff_recv, q_rad, q_con = efficiency(T, q, Tinfr, Tinfc, h, em)
+                
+		self.entry["eff th"].set_text("{0}".format(eff_ther))
+		self.entry["eff re"].set_text("{0}".format(eff_recv))
+                self.entry["q rad"].set_text("{0}".format(q_rad))
+		self.entry["q con"].set_text("{0}".format(q_con))
+                
+                
 	def delete_event(self, widget, event, data=None):
 		# If you return FALSE in the "delete_event" signal handler,
 		# GTK will emit the "destroy" signal. Returning TRUE means
@@ -64,7 +67,26 @@ class HelloWorld:
 	def destroy(self, widget, data=None):
 		print "destroy signal occurred"
 		gtk.main_quit()
+        
+        def label_text(self, vbox, nickname, label, value):
+	        vb = gtk.VBox(False,0)
+		vbox.pack_start(vb, True, True, 0)
+		vb.show()
+		
+		l = gtk.Label(label)
+		vb.pack_start(l, True, True, 0)
+		l.show()
+		
+		e = gtk.Entry(max=0)
+		e.set_text("{0}".format(value))
+		vb.pack_start(e, True, True, 0)
+		e.show()
 
+		self.entry[nickname] = e
+		
+		self.vbox.append(vb)
+
+        
 	def __init__(self):
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("delete_event", self.delete_event)
@@ -87,12 +109,25 @@ class HelloWorld:
 		vbox.pack_start(self.button[1], True, True, 0)
 		self.button[1].show()
 
-		labels = ["temperature (K)","flux (w/m2)","eff thermal","eff receiver"];
-		x = [0,0,0,0]
 		
 		self.vbox = []
-		self.entry = []
-		for i in range(4):
+		self.entry = {}
+
+                self.label_text(vbox, "T", "temperature (K)", 0)
+                self.label_text(vbox, "q", "flux (w/m2)", 0)
+                
+                self.label_text(vbox, "Tinf r", "T inf rad (K)", 298.15)
+                self.label_text(vbox, "Tinf c", "T inf conv (K)", 298.15)
+                self.label_text(vbox, "h", "h_nat (W/m2 K)", 15.0)
+                self.label_text(vbox, "em", "emiss", 0.95)
+                
+                self.label_text(vbox, "eff th", "efficiency thermal", 0)
+                self.label_text(vbox, "eff re", "efficiency receiver", 0)
+                self.label_text(vbox, "q rad", "q rad", 0)
+                self.label_text(vbox, "q con", "q conv", 0)
+               
+                
+		for i in range(0):
 			vb = gtk.VBox(False,0)
 			vbox.pack_start(vb, True, True, 0)
 			vb.show()
@@ -109,12 +144,6 @@ class HelloWorld:
 			
 			self.vbox.append(vb)
 		
-		self.entry_temp = self.entry[0]
-		self.entry_flux = self.entry[1]
-		self.entry_ther = self.entry[2]
-		self.entry_recv = self.entry[3]
-
-
 		
 		# and the window
 		self.window.show()
