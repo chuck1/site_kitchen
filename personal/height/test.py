2e2c66fc2d71723d465d22ddf20f193297d36324
Charles Rymal <charlesrymal@gmail.com>
2014-08-06 20:45:29 -0700

    cleanup and add functionality to personal/height

--- personal/height/test.py
+++ personal/height/test.py
@@ -1,100 +1,88 @@
+#!/usr/bin/env python
+
 import math
 from matplotlib import cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-def fun1(x,r):
-	return r * math.cos(x)
-	
-def fun2(x,r):
-	return r * math.sin(x)
+from height import *
 
-def para1(x,y,nx,ny,r):
-	X = np.zeros((nx,ny))
-	Y = np.zeros((nx,ny))
-	Z = np.zeros((nx,ny))
+def grad(X,Y,Z):
+    dx = X[0,1]-X[0,0]
+    G = np.gradient(Z,dx)
+    g = np.sqrt(np.square(G[0]) + np.square(G[1]))
+    
+    g = g-np.min(g)
+    g = g/np.max(g)
+    
+    return g
 
-	uX = np.zeros((nx,ny))
-	uY = np.zeros((nx,ny))
-	uZ = np.zeros((nx,ny))
-	
-	
-	for i in range(nx):
-		for j in range(ny):
-			X[i,j] = fun1(x[i],r)
-			Y[i,j] = fun2(x[i],r)
-			Z[i,j] = y[j]
-			
-			uX[i,j] = -fun1(x[i],r) / r
-			uY[i,j] = -fun2(x[i],r) / r
-			uZ[i,j] = 0
+def plot(X,Y,Z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    c = ax.contourf(X, Y, Z, 10)
+    plt.colorbar(c)
+    
+    plt.show()
 
-			
-	return X,Y,Z,uX,uY,uZ
+def plot3(X,Y,Z):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+   
+    G = grad(X,Y,Z)
+    
+    ax.plot_surface(X, Y, Z,  rstride=1, cstride=1, facecolors=cm.jet(G))
+    
+    plt.show()
 
-def filt(z,nx,ny,w):
-	Z = np.array(z)
-	
-	a = (2*w + 1)**2
-	
-	for i in range(nx):
-		for j in range(ny):
-			s = 0
-			for i2 in range(i-w,i+w+1):
-				for j2 in range(j-w,j+w+1):
-					s += z[i2 if i2 < nx else i2 - nx,j2 if j2 < ny else j2 - ny]
-			Z[i,j] = s/a
-	return Z
-	
-def crater(x,y,z,nx,ny,X,Y,r,d):
-	
-	for i in range(nx):
-		for j in range(ny):
-			R = math.sqrt((x[i]-X)**2 + (y[j]-Y)**2)
-			if R < r:
-				z[i][j] = d
 
-	return z
+def cylinder(Z):
+    x = np.linspace(0,2*math.pi,nx)
+    y = np.linspace(0,10,ny)
+    
+    X, Y, Z, uX, uY, uZ = cylinder(x,y,1)
+    Z = perturb(X,Y,Z,uX,uY,uZ,pZ,nx,ny)
 
-def perturb(X,Y,Z,uX,uY,uZ,pZ,nx,ny):
+    plot(X,Y,Z)
 
-	s = 5
+def plane(Z):
+    s = np.shape(Z)
+    nx = s[0]
+    ny = s[1]
+    x = np.linspace(0,10,nx)
+    y = np.linspace(0,10,ny)
+    
+    X,Y = np.meshgrid(x,y)
 
-	for i in range(nx):
-		for j in range(ny):
-			X[i,j] += uX[i,j] * pZ[i,j] * s
-			Y[i,j] += uY[i,j] * pZ[i,j] * s
-			Z[i,j] += uZ[i,j] * pZ[i,j] * s
-			
-	return Z
-	
+    return X,Y
+   
 nx = 100
 ny = 100
 
-x = np.linspace(0,2*math.pi,nx+1)
-y = np.linspace(0,10,ny+1)
+Z = np.random.random_sample((nx,ny))
+#Z = np.random.rand(nx,ny)
+#Z = np.zeros((nx,ny))
+
+X,Y = plane(Z)
+
+Z = filtw(Z,20)
+
+#Z = crater(X,Y,Z,5,5,2,0.4)
+
+#Z = filt(Z,5)
+
+Z = norm(Z)
+
+Z = below(Z,0.5)
+
+plot(X,Y,Z)
+
+#plot3(X,Y,Z)
 
 
-pZ = np.random.rand(nx+1,ny+1)
-
-
-
-pZ = filt(pZ,nx+1,ny+1,5)
-
-#pZ = crater(x,y,Z,nx+1,ny+1,5,5,1,0.4)
-
-pZ = filt(pZ,nx+1,ny+1,5)
-
-X, Y, Z, uX, uY, uZ = para1(x,y,nx+1,ny+1,5)
-
-Z = perturb(X,Y,Z,uX,uY,uZ,pZ,nx+1,ny+1)
-
-fig = plt.figure()
-ax = Axes3D(fig)
 
 #surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='RdBu', linewidth=0, antialiased=False)
-ax.plot_surface(X, Y, Z,  rstride=1, cstride=1, color='b')
-plt.show()
 
 
