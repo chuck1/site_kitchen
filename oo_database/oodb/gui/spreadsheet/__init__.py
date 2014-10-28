oodb: fixing structure; getting units into plot

oo_database/oodb/gui/spreadsheet/__init__.py
@@ -4,214 +4,32 @@ from PyQt4 import QtGui, QtCore
 import pylab as pl
 
 import oodb
+import oodb.gui.objectedit
 
-
+from oodb.gui.spreadsheet.Table import Table
 
 class Format:
-        def prnt(self, value):
-                data = value.prnt()
+	def prnt(self, value):
+		data = value.get()
+		
+		if isinstance(data, str):
+				return '{}'.format(data)
+		elif isinstance(data, int):
+				return '{}'.format(data)
+		elif isinstance(data, float) or isinstance(data, np.float64):
+				return '{:E}'.format(data)
+		elif isinstance(data, type):
+				return '{}'.format(data)
 
-                if isinstance(data, str):
-                        return '{}'.format(data)
-                elif isinstance(data, int):
-                        return '{}'.format(data)
-                elif isinstance(data, float) or isinstance(data, np.float64):
-                        return '{:E}'.format(data)
-                elif isinstance(data, type):
-                        return '{}'.format(data)
+		return str(data)
+		
+		print(type(value))
+		print(type(data))
+		raise Exception('Format.prnt')
 
-                return str(data)
-                
-                print(type(value))
-                print(type(data))
-                raise Exception('Format.prnt')
 
-class Table(QtGui.QTableWidget):
-        def __init__(self, parent):
-                super(Table, self).__init__(parent)
-
-                self.itemChanged.connect(self.handleItemChanged)
-                self.itemSelectionChanged.connect(self.handleItemSelectionChanged)
-
-                self.windows = []
         
-        def event(self, e):
-                #print('QtGui.QTableWidget:', e)
-                return super(Table, self).event(e)
 
-        def editObject(self):
-                print('editObject')
-                
-                for rng in self.selectedRanges():
-                        #for c in range(rng.leftColumn(), rng.rightColumn() + 1):
-                        for r in range(rng.topRow(), rng.bottomRow() + 1):
-                                print('r',r)
-                                obj = self.objectAt(r)
-                                print(obj)
-                                if obj:
-                                        self.windows.append(oodb.gui.objectedit.Window(obj))
-                                        
-
-        def refresh(self):
-
-                text1 = self.parent().editext[0].text()
-                print('text1',repr(text1))
-
-                text2 = "[" + self.parent().text_fields.toPlainText() + "]"
-                print('text1',repr(text2))
-                
-                if text1 and text2:
-                        try:
-                                res1 = eval(text1)
-                                res2 = eval(text2)
-                                #print(res)
-                                self.rows = self.parent().db.gen_rows(res1, res2)
-                        except:
-                                self.setRowCount(0)
-                                self.setColumnCount(0)
-                                return
-                else:
-                        #self.rows = self.db.gen_rows(oodb.class_util.all, ['id', ('type',type), 'desc'])
-                        #self.rows = self.db.gen_rows(
-                        #        oodb.class_util.designs,
-                        #        ['id', ('type',lambda x: str(type(x))), 'Re', 'mdot']
-                        #        )
-                        self.rows = []
-                        self.setRowCount(0)
-                        self.setColumnCount(0)
-                        return
-                
-                
-                #print(self.rows)
-
-                R = len(self.rows)
-                C = len(self.rows[0])
-
-                fmt = [Format()]*C
-                
-                self.setRowCount(R)
-                self.setColumnCount(C)
-
-                for r in range(R):
-                        for c in range(C):
-                                v = self.rows[r][c]
-
-                                if v.editable:
-                                        i = oodb.gui.TableWidgetItemRaw(
-                                                self.rows[0][c].prnt(),
-                                                v.prnt(),
-                                                v.obj)
-                                else:
-                                        i = oodb.gui.TableWidgetItem(
-                                                fmt[c].prnt(v)
-                                                )
-
-                                #if isinstance(v, oodb.Value):
-                                #print('WhatsThis',str(v.obj))
-
-                                d = v.prnt()
-                                
-                                i.setToolTip(str(type(d)))
-                                
-                                i.setEditable(v.editable)
-                                
-                                self.setItem(r,c,i)                        
-
-                self.resizeColumnsToContents()
-        
-        def contextMenuEvent(self, event):
-            self.menu = QtGui.QMenu(self)
-
-            editobjectAction = QtGui.QAction('Edit Object', self)
-            editobjectAction.triggered.connect(self.editObject)
-            
-            self.menu.addAction(editobjectAction)
-            # add other required actions
-            self.menu.popup(QtGui.QCursor.pos())
-
-        def plot(self):
-
-                ret = self.selectionArray()
-
-                if ret:
-                        rows = ret[0]
-                        cols = ret[1]
-                        arr = ret[2]
-                        
-                        print('cols',cols)
-                        print('arr')
-                        
-                        for a in arr:
-                                print(a)
-                        
-                        #print(arr[0])
-                        pl.plot(arr[0], arr[1], 'o')
-                        
-                        pl.xlabel(self.item(0, cols[0]).text())
-                        pl.ylabel(self.item(0, cols[1]).text())
-                        
-                        pl.show()
-
-        def selectionArray(self):
-                l = self.selectedRanges()
-                
-                ret = oodb.util.processRange(l)
-
-                if ret:
-                        rows = ret[0]
-                        cols = ret[1]
-
-                        print('rows cols', rows, cols)
-                        
-                        nRows = len(rows)
-                        nCols = len(cols)
-
-                        arr = []
-                        for c in range(nCols):
-                                arr.append(np.zeros((nRows)))
-                        
-                        for r in range(nRows):
-                                for c in range(nCols):
-                                        #print('r c', r, c)
-                                        R = rows[r]
-                                        C = cols[c]
-                                        #print('R C', R, C)
-                                        
-                                        i = self.item(R, C)
-                                        #print(i.text())
-                                        arr[c][r] = float(i.text())
-                
-                        return rows, cols, arr
-                
-                return None
-        
-        def handleItemSelectionChanged(self):
-                print('QtGui.QTableWidget: itemSelectionChanged')
-
-        def objectAt(self, r):
-                v = self.rows[r][0]
-                if isinstance(v, oodb.Value):
-                        return v.obj
-
-                return None
-                
-        def handleItemChanged(self, item):
-                if isinstance(item, oodb.gui.TableWidgetItemRaw):
-                        item.handleChanged()
-                        return
-                
-                #print(item, 'changed')
-                #print(self.parent())
-                
-                v = self.rows[item.row()][item.column()]
-                #print(v)
-                
-                if isinstance(v, oodb.Value) and v.editable:
-                        #print('edit value')
-                        setattr(
-                                v.obj,
-                                v.name,
-                                oodb.gui.convert(item.text()))
 
 
 class TabLayout(QtGui.QVBoxLayout):
@@ -298,7 +116,7 @@ class TabWidget(QtGui.QTabWidget):
                 self.s0 = s0
                 self.s1 = s1
                 
-                self.setStyleSheet('font-size: 11pt; font-family: Courier;')
+                #self.setStyleSheet('font-size: 11pt; font-family: Courier;')
 
                 self.setTabPosition(QtGui.QTabWidget.South)
 
@@ -358,9 +176,11 @@ class TabWidget(QtGui.QTabWidget):
         def createMenu(self):
                 pass
 
-class Window(QtGui.QWidget):
+class Window(oodb.gui.Window):
         def __init__(self, s0, s1):
                 super(Window, self).__init__()
+
+                
 
                 self.layout = QtGui.QVBoxLayout()
 
@@ -390,6 +210,11 @@ class Window(QtGui.QWidget):
 
         def createActions(self):
 
+                saveAction = QtGui.QAction(QtGui.QIcon('save.png'), '&Save', self)        
+                saveAction.setShortcut('Ctrl+S')
+                saveAction.setStatusTip('Save')
+                saveAction.triggered.connect(self.tabwidget.currentWidget().db.save)
+
                 exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)        
                 exitAction.setShortcut('Ctrl+Q')
                 exitAction.setStatusTip('Exit application')
@@ -408,8 +233,9 @@ class Window(QtGui.QWidget):
                 #self.statusBar()
                 
                 fileMenu = self.menubar.addMenu('&File')
+                fileMenu.addAction(saveAction)
                 fileMenu.addAction(exitAction)
-
+                
                 toolsMenu = self.menubar.addMenu('&Tools')
                 toolsMenu.addAction(refreshAction)
                 toolsMenu.addAction(plotAction)