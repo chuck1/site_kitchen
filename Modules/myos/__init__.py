dd86ed95447a251e158d39ae2359552392f46dc5
chuck <charlesrymal@gmail.com>
2014-11-19 12:13:10 -0800

    minor

--- Modules/myos/__init__.py
+++ Modules/myos/__init__.py
@@ -6,14 +6,10 @@ def glob(patstr, search = "."):
 	pat = re.compile(patstr)
 
 	for root, dirs, files in os.walk(search):
-
 		#print root, files, dirs
-		
 		for f in files:
 			f = os.path.join(root, f)
-
 			#print f
-
 			if pat.match(f):
 				yield f
 
